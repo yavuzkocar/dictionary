d776d8818dd7913c98b5acab61c081886a64e6c0
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-12-23 22:05:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Collections.Generic
+            Dictionary<int,string> kullanıcılar= new Dictionary<int, string>();
+            kullanıcılar.Add(10,"yavuz kocar");
+            kullanıcılar.Add(12,"bilal kocar");
+            kullanıcılar.Add(16,"merve kocar");
+            kullanıcılar.Add(13,"oznur kocar");
+            
+
+            //dizinin elemanlarına erişim
+            Console.WriteLine("****dizinin elemanlarına erisim****");
+            Console.WriteLine(kullanıcılar[12]);
+
+            //count eleman sayısı
+             Console.WriteLine("****count****");
+            Console.WriteLine(kullanıcılar.Count);
+            
+            //contains - arama true veya false döner
+             Console.WriteLine("****contains****");
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("yavuz kocar"));
+            //Remove-listeden çıkarma
+            Console.WriteLine("****remove****");
+            kullanıcılar.Remove(12);
+            foreach (var item in kullanıcılar)
+                Console.WriteLine(item.Value);
+
+            //keys
+            Console.WriteLine("****keys****");
+            foreach (var item in kullanıcılar.Keys)
+            Console.WriteLine(item);
+    
+            //values
+            Console.WriteLine("****values****");
+            foreach (var item in kullanıcılar.Values)
+            Console.WriteLine(item);
+
+
+        }
+    }
+}
